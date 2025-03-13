d50ee0900365fdd8ac289692e32a6ffbb1ff02c7
haohaounique <haohaounique@163.com>
2025-03-13 22:47:58 +0800

    gradle config

---
+++ src/main/java/com/unique/rule/check/RuleCheckApplication.java
@@ -0,0 +1,19 @@
+package com.unique.rule.check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * date:2025/1/26 13:20
+ * author: haohaounique@163.com
+ */
+@SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
+public class RuleCheckApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RuleCheckApplication.class, args);
+    }
+}
