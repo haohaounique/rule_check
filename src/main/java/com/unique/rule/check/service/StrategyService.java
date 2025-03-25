7cccd56fc251a9582a3ac90e1169bd9b782dcc23
haohaounique <haohaounique@163.com>
2025-03-25 20:26:50 +0800

    Strategy

--- src/main/java/com/unique/rule/check/service/StrategyService.java
+++ src/main/java/com/unique/rule/check/service/StrategyService.java
@@ -5,7 +5,9 @@ import com.unique.framework.common.http.exception.GlobalException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * date:2025/3/15 16:13
@@ -15,8 +17,16 @@ import java.util.List;
 public class StrategyService {
 
 
+    // 新增一个 Map 用于存储 AbstractCheckService 实例，键为类的简单名称
+    private final static Map<String, AbstractCheckService> CHECK_SERVICE_HASH_MAP = new HashMap<>();
+
+    // 在构造函数中初始化 Map
     @Autowired
-    private List<AbstractCheckService> checkServiceList;
+    public StrategyService(List<AbstractCheckService> checkServiceList) {
+        for (AbstractCheckService service : checkServiceList) {
+            CHECK_SERVICE_HASH_MAP.put(service.getClass().getSimpleName(), service);
+        }
+    }
 
     /**
      * 校验服务类
@@ -25,10 +35,13 @@ public class StrategyService {
      * @return AbstractCheckService
      */
     public AbstractCheckService getCheckService(String clazzName) {
-        return checkServiceList.stream()
-                .filter(item -> item.getClass().getSimpleName().equals(clazzName))
-                .findFirst()
-                .orElseThrow(() -> new GlobalException(GlobalErrorCode.FAIL,"未找到类名为 " + clazzName + " 的 AbstractCheckService 实例"));
+        // 提前对 clazzName 进行空值检查，避免空指针异常
+        if (clazzName == null || clazzName.isEmpty()) {
+            throw new GlobalException(GlobalErrorCode.FAIL, "传入的类名不能为空");
+        }
+        // 直接从 Map 中获取实例
+        return CHECK_SERVICE_HASH_MAP.computeIfAbsent(clazzName, key -> {
+            throw new GlobalException(GlobalErrorCode.FAIL, "未找到类名为 " + clazzName + " 的 AbstractCheckService 实例");
+        });
     }
-
 }
